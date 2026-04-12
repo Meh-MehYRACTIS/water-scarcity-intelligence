2a459a72965f98ac0e5ad7de378e7721aedef416
Meh-MehYRACTIS <trippjm1309@gmail.com>
2026-04-12 10:35:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -11,5 +11,5 @@
 //   6. Save this file, commit, and push to GitHub
 // ============================================================
 
-var SUPABASE_URL      = 'https://ymaqwmvubkgslrghwveg.supabase.co';       // e.g. https://xxxxxxxxxxxx.supabase.co
+var SUPABASE_URL      = 'https://ymaqwmvubkgslrghwveg.supabase.co';  
 var SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltYXF3bXZ1Ymtnc2xyZ2h3dmVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5NzE3NzIsImV4cCI6MjA5MTU0Nzc3Mn0.kvOFrcQnYe_VCCk_sjWKnoMUBoTKRTbDXPMNt3L8B7k';
